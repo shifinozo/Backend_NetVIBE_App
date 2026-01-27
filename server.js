0060ae1453e95630f915934ccfa4af37c579d8ec
muhammed shifin <shifinshifu246@gmail.com>
2026-01-27 12:13:53 +0530

    Fix Render deployment issue

--- server.js
+++ server.js
@@ -147,10 +147,9 @@ io.on("connection", (socket) => {
 });
 
 
-// server.listen(5000, () => {
-  // console.log("🚀 Backend running on http://localhost:5000");
-// });
+server.listen(5000, () => {
+  console.log("🚀 Backend running on http://localhost:5000");
+});
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
