af65322f414347592071717ac4c94e7f8c799007
muhammed shifin <shifinshifu246@gmail.com>
2026-02-04 11:04:56 +0530

    Fix import path case issue

--- server.js
+++ server.js
@@ -68,11 +68,11 @@ const server = http.createServer(app);
 /* 🔥 SOCKET.IO SETUP */
 export const io = new Server(server, {
   cors: {
-    // origin: "http://localhost:5173",
-    origin: [
-  "http://localhost:5173",
-  "https://your-frontend.vercel.app"
-],
+    origin: "http://localhost:5173",
+//     origin: [
+//   "http://localhost:5173",
+//   "https://your-frontend.vercel.app"
+// ],
 
     credentials: true,
   },
